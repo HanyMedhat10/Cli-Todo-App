8d04f149438f2da9cd7e4f5e49b87f62b4e6c381
Hany Medhat <hany.medhat24@gmail.com>
2025-08-25 20:58:16 +0300

    Add initial project files: .gitignore, go.mod, main.go, and todo.go with basic functionality

--- main.go
+++ main.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"fmt"
-	// "os"
 )
 
 func main() {
 	fmt.Println("Inside main in todo app")
+	todos := Todos{}
+	todos.Add("Learn Go")
+	todos.Add("Build a CLI app")
+	fmt.Printf("%+v\n\n", todos)
+	todos.delete(0)
+	fmt.Printf("\n\n after deletion: 1 \n")
+	fmt.Printf("%+v\n\n", todos)
+	todos.delete(0)
+	fmt.Printf("\n\n after deletion: 2 \n")
+	fmt.Printf("%+v\n\n", todos)
+
 }
--- todo.go
+++ todo.go
@@ -1,6 +1,10 @@
 package main
 
-import "time"
+import (
+	"errors"
+	"fmt"
+	"time"
+)
 
 type Todo struct {
 	ID        int
@@ -11,3 +15,45 @@ type Todo struct {
 }
 
 type Todos []Todo
+
+func (todos *Todos) Add(title string) {
+	now := time.Now()
+	todo := Todo{
+		ID:        len(*todos) + 1,
+		Title:     title,
+		CreatedAt: &now,
+		UpdatedAt: &now,	
+		Completed: false,
+	}
+	*todos = append(*todos, todo)
+}
+
+func (todos *Todos) List() Todos {
+	return *todos
+}
+func (todos *Todos) find(index int) (*Todo, error) {
+	for i, todo := range *todos {
+		if i == index {
+			return &todo, nil
+		}
+	}
+	return nil, errors.New("Todo not found")
+}
+
+func (todos *Todos) validateIndex(index int) error {
+	if index < 0 || index >= len(*todos) {
+		err := errors.New("Index out of range") /* invalid index */
+		fmt.Println(err)
+		return err 
+	}
+	return nil	
+}
+
+func (todos *Todos) delete(index int) error {
+	if err := todos.validateIndex(index); err != nil {
+		return err
+	}
+	// Delete the todo at the specified index before and after the validation
+	*todos = append((*todos)[:index], (*todos)[index+1:]...)
+	return nil
+}
